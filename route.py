a454098d21fa025f6d4d16b82fa832028f1ef2db
Exoreath <102925930@student.swin.edu.au>
2022-10-26 23:52:28 +1100

    functioning pathfinding

--- route.py
+++ route.py
@@ -1,7 +1,10 @@
 from array import array
 from asyncio.windows_events import NULL
+from codecs import unicode_escape_decode
+import heapq
 import math
 from tabnanny import check
+from turtle import distance
 import warnings
 import numpy as np
 import pandas as pd
@@ -11,12 +14,13 @@ from keras.utils.vis_utils import plot_model
 import sklearn.metrics as metrics
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import sys
 warnings.filterwarnings("ignore")
 
 # Data class that holds values for route creation
 class coordData:
     def __init__(self, scatsNumber, streets, latitude, longitude, id):
-        neighbours = []
+        self.neighbours = []
 
         self.scatsNumber = scatsNumber
         self.street1 = streets.split(" ")[0]
@@ -24,15 +28,46 @@ class coordData:
         self.latitude = latitude
         self.longitude = longitude
         self.id = id
+        self.visited = False
+        self.distance = sys.maxsize
+        self.weight = 0
+        self.previous = None
+    
+    def setVisited(self, bool):
+        self.visited = bool
+
+    def getDistance(self):
+        return self.distance
+
+    def setDistance(self, dist):
+        self.distance = dist
+
+    def getWeight(self, node):
+        for x in self.neighbours:
+            if x == node:
+                return calculateWeight(self, node)
+        #return self.neighbours[node].weight
+
+    def setPrevious(self, prev):
+        self.previous = prev
 
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ - setting up neighbours
 
+def calculateWeight(current, neighbour):
+    weight = distanceBetweenVectors(current, neighbour)
+    return weight
+
+
 def findNeighbours(dataList):
     for x in dataList:
-        x.neighbours = getScatsOnSameRoad(dataList, x.street1, x.street2, x.id)
+        x.neighbours = getScatsOnSameRoad(dataList.copy(), x.street1, x.street2, x.id)
+
+    # for x in dataList:
+    #     x.neighbours = getClosestNeighbours(x)
 
     for x in dataList:
-        x.neighbours = getClosestNeighbours(x)
+        if len(x.neighbours) == 0:
+            x.neighbours = neighboursForNull(dataList, x)
 
     return dataList
 
@@ -51,12 +86,12 @@ def getClosestNeighbours(node):
     for x in node.neighbours:
         result = getNeighbourDirection(node, x)
         if result:
-            horizontalNeighbours.append(x)
+            verticalNeighbours.append(x)            
         else:
-            verticalNeighbours.append(x)
+            horizontalNeighbours.append(x)
 
-    neighbours = reduceHorizontalNeighbours(horizontalNeighbours, node.latitude)
-    temp = reduceVerticalNeighbours(verticalNeighbours, node.longitude)
+    neighbours = reduceHorizontalNeighbours(horizontalNeighbours, node.longitude)
+    temp = reduceVerticalNeighbours(verticalNeighbours, node.latitude)
     for x in temp:
         neighbours.append(x)
 
@@ -67,43 +102,43 @@ def getNeighbourDirection(node, nodeNeighbour):
     dif2 = nodeNeighbour.longitude - node.longitude
 
     if abs(dif1) > abs(dif2):
-        return True #horizontal
+        return True #vertical
     else:
-        return False #vertical
+        return False #horizontal
 
-def reduceHorizontalNeighbours(arrayOfNeighbours, nodeLatitude):
+def reduceHorizontalNeighbours(arrayOfNeighbours, nodeLongitutde):
     left = []
     right = []
     closestLeft = NULL
     closestRight = NULL
     for x in arrayOfNeighbours:
-        if x.latitude < nodeLatitude:
+        if x.longitude < nodeLongitutde:
             left.append(x)
         else:
             right.append(x)
     for x in left:
-        if closestLeft == NULL or abs(nodeLatitude) - abs(x.latitude) < abs(nodeLatitude) - abs(closestLeft.latitude):
+        if closestLeft == NULL or abs(nodeLongitutde - x.longitude) < abs(nodeLongitutde - closestLeft.longitude):
             closestLeft = x
     for x in right:
-        if closestRight == NULL or abs(x.latitude) - abs(nodeLatitude) < abs(closestRight.latitude) - abs(nodeLatitude):
+        if closestRight == NULL or abs(x.longitude - nodeLongitutde) < abs(closestRight.longitude - nodeLongitutde):
             closestRight = x
     return checkIfNullForArray(closestLeft, closestRight)
     
-def reduceVerticalNeighbours(arrayOfNeighbours, nodeLongitutde):
+def reduceVerticalNeighbours(arrayOfNeighbours, nodeLatitude):
     down = []
     up = []
     closestDown = NULL
     closestUp = NULL
     for x in arrayOfNeighbours:
-        if x.longitude < nodeLongitutde:
+        if x.latitude < nodeLatitude:
             down.append(x)
         else:
             up.append(x)
     for x in down:
-        if closestDown == NULL or abs(nodeLongitutde) - abs(x.longitude) < abs(nodeLongitutde) - abs(closestDown.longitude):
+        if closestDown == NULL or abs(nodeLatitude - x.latitude) < abs(nodeLatitude - closestDown.latitude):
             closestDown = x
     for x in up:
-        if closestUp == NULL or abs(x.longitude) - abs(nodeLongitutde) < abs(closestUp.longitude) - abs(nodeLongitutde):
+        if closestUp == NULL or abs(x.latitude - nodeLatitude) < abs(closestUp.latitude - nodeLatitude):
             closestUp = x
     return checkIfNullForArray(closestDown, closestUp)
 
@@ -114,6 +149,76 @@ def checkIfNullForArray(value1, value2):
     if value2 != NULL:
         array.append(value2)
     return array
+
+def neighboursForNull(dataList, node):
+    closestNeighbour = dataList[0]
+    closestDistance = distanceBetweenVectors(node, closestNeighbour)
+    for x in dataList:
+        tempDistance = distanceBetweenVectors(node, x)
+        if tempDistance < closestDistance and tempDistance != 0:
+            closestNeighbour = x
+            closestDistance = tempDistance
+
+    secondClosestNeighbour = NULL
+    secondClosestDistance = NULL
+    for x in dataList:
+        tempDistance = distanceBetweenVectors(node, x)
+        if (tempDistance < secondClosestDistance or secondClosestDistance == NULL) and x != closestNeighbour and tempDistance != 0:
+            secondClosestNeighbour = x
+            secondClosestDistance = tempDistance
+    
+    return [closestNeighbour, secondClosestNeighbour]
+
+def distanceBetweenVectors(node1, node2):
+    distance = math.sqrt((node1.latitude - node2.latitude)**2 + (node1.longitude - node2.longitude)**2)
+    return distance
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+def shortest(target, path):
+    if target.previous:
+        path.append(target.previous.id)
+        shortest(target.previous, path)
+    return
+
+def dijkstra(dataList, startNode, endNode):
+    #set distance of start node to 0
+    startNode.setDistance(0)
+
+    # Put tuple pair into the priority queue
+    unvisitedQueue = [(x.getDistance(), x.id, x) for x in dataList]
+    heapq.heapify(unvisitedQueue)
+
+    while len(unvisitedQueue):
+        # Pops a vertex with the smallest distance 
+        uv = heapq.heappop(unvisitedQueue)
+        current = uv[2]
+        current.setVisited(True)
+
+        #for next in node's neighbours
+        for next in current.neighbours:
+            #if visited, skip
+            if next.visited:
+                continue
+            newDist = current.getDistance() + current.getWeight(next)
+
+            if newDist < next.getDistance():
+                next.setDistance(newDist)
+                next.setPrevious(current)
+                print ('updated : current = %s next = %s new_dist = %s' \
+                        %(current.scatsNumber, next.scatsNumber, next.getDistance()))
+            #else:
+                # print ('not updated : current = %s next = %s new_dist = %s' \
+                #         %(current.scatsNumber, next.scatsNumber, next.getDistance()))
+
+        #rebuild heap
+        #pop every item
+        while len(unvisitedQueue):
+            heapq.heappop(unvisitedQueue)
+        #put all vertices not visited into the queue
+        unvisitedQueue = [(v.getDistance(), v.id, v) for v in dataList if not v.visited]
+        heapq.heapify(unvisitedQueue)   
+    
 
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
@@ -138,7 +243,7 @@ def main():
     j = 0
     while i < len(scatsNumber):
         if scatsNumber[i] in scatsList:
-            dataList.append(coordData(scatsNumber[i], streets[i], round(latitude[i] + 0.00155, 5), round(longitude[i] + 0.00113, 5), j))
+            dataList.append(coordData(scatsNumber[i], streets[i], round(latitude[i], 5), round(longitude[i], 5), j))
             scatsList.remove(scatsNumber[i])
             j+=1
         i+=1
@@ -152,9 +257,9 @@ def main():
         print(f'{x.street1} : {x.street2}')
         print(x.latitude)
         print(x.longitude)   
-        print(f'id: {x.id}')
+        print(f'id: {x.id}')    
 
-    dataList = findNeighbours(dataList)
+    dataList = findNeighbours(dataList.copy())
     for x in dataList:
         print('--------------------------')
         if len(x.neighbours) == 0:
@@ -162,6 +267,17 @@ def main():
         for y in x.neighbours:
             print(f'{x.scatsNumber}: {y.scatsNumber}')
     #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+    dijkstra(dataList, dataList[34], dataList[30])
+    print(dataList[34].scatsNumber, dataList[30].scatsNumber)
+
+    target = dataList[30]
+    path = [target.id]
+    shortest(target, path)
+    print(('The shortest path : %s' %(path[::-1])))
+
+    # for x in dataList:
+    #     print(f'{x.id} : {x.scatsNumber}')
 
 if __name__ == '__main__':
     main()
